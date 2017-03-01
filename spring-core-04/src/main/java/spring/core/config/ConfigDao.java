9b824dd8174a9c2044cba3e9c533ee9e48e21756
Viorel Florica <viorelflorica@outlook.com>
2017-03-01 15:53:02 +0100

    Modification de la classe ConfigDao, il rest à completer avec les todo !

--- spring-core-04/src/main/java/spring/core/config/ConfigDao.java
+++ spring-core-04/src/main/java/spring/core/config/ConfigDao.java
@@ -1,19 +1,24 @@
 package spring.core.config;
 
+import java.io.IOException;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import spring.core.dao.ElectionsDaoFile;
 import spring.core.dao.IElectionsDao;
+import spring.core.dao.entities.ElectionsException;
+import spring.core.dao.ElectionsDaoFile;
 
 @Configuration
-@ComponentScan({ "spring.core.entities" })
+@ComponentScan({ "elections.dao.entities" })
 public class ConfigDao {
-	
+	// implémentation 1
 	@Bean
-	public IElectionsDao ElectionsDaoFile(){
-		return new ElectionsDaoFile("data\\elections-in-good.txt", "data\\elections-out.txt", "data\\elections-log.txt");
+	public IElectionsDao electionsDaoFile() throws ElectionsException, IOException {
+		return new ElectionsDaoFile("elections-in-good.txt","elections-out.txt","elections-log.txt");
+		
 	}
+	
 	
 }
